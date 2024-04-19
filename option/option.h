a5fe342ec23dd44479bd0cd93819f68ac59cc9d6
Dinesh Kumar <gdineshda@gmail.com>
2024-04-19 21:39:12 +0530

    Option: go rust or go bust

--- option/option.h
+++ option/option.h
@@ -13,11 +13,20 @@ typedef struct {
 	void *data;
 } option;
 
+typedef void* (*transform) (void *);
+
 option some(void *data);
 option none(void);
+
+option is_some_and(option o, transform fun);
+option is_none_and(option o, transform fun);
+
 bool is_some(option o);
 bool is_none(option o);
-void *get_some(option o);
+
+// TODO: Implement this
+void *unwrap(option o);
+void *unwrap_or(option o, void *default_value);
 
 #ifdef OPTION_IMPL
 
@@ -43,7 +52,25 @@ bool is_none(option o) {
 	return o.t == NONE;
 }
 
-void *get_some(option o) {
+option is_some_and(option o, transform fun) {
+	if (is_some(o)) {
+		o.data = fun(o.data);
+		return o;
+	} else {
+		return o;
+	}
+}
+
+option is_none_and(option o, transform fun) {
+	if (is_none(o)) {
+		o.data = fun(o.data);
+		return o;
+	} else {
+		return o;
+	}
+}
+
+void *unwrap(option o) {
 	return o.data;
 }
 
